c397d1da16fb8b9ebbb58196281e2fa30c8307e7
Jakub Kuka <jakubkuka07@gmail.com>
2025-04-15 15:47:08 +0900

    Previous contents deleted

--- alpaca_api.py
+++
@@ -1,9 +0,0 @@
-from alpaca_trade_api import REST
-api = REST("API_KEY", "API_SECRET", base_url="https://paper-api.alpaca.markets")
-
-# Get Account Balance
-account = api.get_account()
-print(f"Account Equity: ${account.equity}")
-
-# Place a Market Order (Buying 10 shares of AAPL)
-api.submit_order(symbol="AAPL", qty=10, side="buy", type="market", time_in_force="gtc")
--- websockets.py
+++
@@ -1,9 +0,0 @@
-import websocket
-import json
-
-def on_message(ws, message):
-    print(f"Received: {message}")
-
-ws = websocket.WebSocketApp("wss://stream.data.alpaca.markets/v2/sip",
-                             on_message=on_message)
-ws.run_forever()
